19dafd5e8d1857d439f6b8884e11059a6c8a330d
Michael Miles <michael.gage.miles@gmail.com>
2019-03-31 00:20:50 -0700

    created collection view and edited layout

--- ColleckshunView/ViewController.swift
+++ ColleckshunView/ViewController.swift
@@ -8,13 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+//    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let backgroundColors = [UIColor.black, UIColor.blue, UIColor.red, UIColor.brown, UIColor.green, UIColor.gray]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        collectionView.dataSource = self
+//        collectionView.delegate = self
     }
-
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return backgroundColors.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "demoCell", for: indexPath)
+        
+        cell.backgroundColor = backgroundColors[indexPath.item]
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width/2
+        let height = width * 2
+        
+        return CGSize(width: width, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
 
 }
 
